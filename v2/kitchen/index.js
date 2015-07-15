ed7a2a3316ff7a8b8409b2f39be6716b56575dca
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2015-07-15 14:26:11 -0700

    Basic throttling and direction checking

--- v2/kitchen/index.js
+++ v2/kitchen/index.js
@@ -1,7 +1,35 @@
 Leap = require('leapjs');
+_ = Leap._;
 
 console.log('hello world');
 
-Leap.loop(function(frame){
-  console.log('frame', frame.id);
+Leap.loop({enableGestures: true});
+
+
+
+
+var Speaker = function(){
+};
+Speaker.prototype = {
+
+  sayNextLine: _.throttle(function(gesture){
+    console.log('now speaking. right: ', gesture.direction[0] > 0);
+  }, 2000, {trailing: false})
+
+};
+
+
+speaker = new Speaker();
+
+Leap.loopController.on('gesture', function(gesture){
+  if (gesture.type != 'swipe') return;
+
+  if (
+    Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]) &&
+    Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[2])
+  ){
+    //console.log('gesture', arguments);
+    speaker.sayNextLine(gesture);
+  }
+
 });
